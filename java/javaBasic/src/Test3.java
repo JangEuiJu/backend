a22194732332d6f1ec7c04e5805491169ba91e17
JangEuiJu <dmlwn3621@naver.com>
2024-12-26 16:10:11 +0900

    Feat: 흐름제어

--- java/javaBasic/src/Test3.java
+++ java/javaBasic/src/Test3.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+/**
+ * 흐름제어
+ *  - 조건문 : if ~ else if ~ else ~
+ *      - 콘솔 입력 사용
+ *  - 반복문 : for ~,  do ~ while, while ~ , continue, break
+ *  - 게임 제작(간단하게) - 스킵
+ *      - 콘솔 입력 사용
+ */
 
 public class Test3 {
+    public static void main(String[] args) {
+        // 조건문 연습
+        flowcontrol1();
+        // 반복문-for 연습 : 지정된 횟수 반복
+        //flowcontrol2();
+        // 반복문-while 연습 : 무한루프(종료 시점 알수 없다)
+        //flowcontrol3();
+    }
 }
