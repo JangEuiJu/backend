60118709bc1147add207949e2ad40d9410ab0b5e
JangEuiJu <dmlwn3621@naver.com>
2024-12-27 23:23:43 +0900

    Feat: SB-기본 프로젝트

--- springboot/demoex/src/main/java/org/example/demoex/DemoexApplication.java
+++ springboot/demoex/src/main/java/org/example/demoex/DemoexApplication.java
@@ -1,4 +1,4 @@
-package com.example.demoex;
+package org.example.demoex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springboot/demoex/src/main/java/org/example/demoex/controllers/HomeController.java
+++ springboot/demoex/src/main/java/org/example/demoex/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.demoex.controllers;
+package org.example.demoex.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
