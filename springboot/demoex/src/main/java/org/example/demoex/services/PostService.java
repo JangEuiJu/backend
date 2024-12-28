fa5029094132f0efd46cff0caf7f6ae00b685ec2
JangEuiJu <dmlwn3621@naver.com>
2024-12-29 03:52:26 +0900

    Feat: SB JPA 게시판 상세보기 적용

--- springboot/demoex/src/main/java/org/example/demoex/services/PostService.java
+++ springboot/demoex/src/main/java/org/example/demoex/services/PostService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 서비스, 비지니스 로직 처리, 주로 SQL 실행 명령을 내리는 곳
@@ -45,5 +46,27 @@ public class PostService {
 
         // 4. 반환
         return postDtos;
+    }
+
+    // 레포지토리를 이용하여 1개의 포스트 객체를 획득 -> DTO 교환 후 반환
+    // select * from post where id='x'; // 무조건 0~1개만 결과
+    public PostDto getOnePost(Integer id) {
+        // Optional는 옵션 => 결과셋이 있을 수도 없을 수도 있다.
+        // 레포지토리는 이미 준비된 메소드를 사용
+        Optional<Post> oPost = this.postRepository.findById(id);
+        if (oPost.isPresent()) {
+            // Post 엔티티 -> PostDto로 변환
+            Post post = oPost.get();
+            return PostDto.builder()
+                    .id(post.getId())
+                    .subject(post.getSubject())
+                    .content(post.getContent())
+                    .createDate(post.getCreateDate())
+                    .reviews(post.getReviews())
+                    .build();
+        }
+        // 없을 경우 -> 던지는 것으로 정리
+        // throw new Exception();
+        return null; // 향후 커스텀 예외처리 변경
     }
 }
